6345567c40d4abb79f833e848c0ed4269e41c059
Michael Wolfindale <mwolfindale@gmail.com>
2015-07-06 19:01:37 +0100

    Add test LDAP server IP to hosts file For use with https://github.com/cforcey/vagrant_ubuntu_openldap

--- scripts/ldap.sh
+++ scripts/ldap.sh
@@ -1,1 +1,6 @@
+# Install and enable PHP LDAP extension.
 apt-get install -y php5-ldap
+
+# Add test LDAP server IP to hosts.
+# Uses https://github.com/cforcey/vagrant_ubuntu_openldap
+echo 192.168.33.20 ldap >> /etc/hosts
